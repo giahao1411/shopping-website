e1ab17e0f4c7d60e1cc09890f9e06ca2ba368129
giahao1411 <trangiahao498@gmail.com>
2024-11-20 15:41:40 +0700

    gather running command on the root folder

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -10,15 +10,13 @@ const App = () => {
         <Router>
             <div>
                 <Routes>
-                    // account route
+                    <Route path="/admin/home" element={<Admin />} />
                     <Route path="/account/login" element={<Login />} />
                     <Route path="/account/register" element={<Register />} />
                     <Route
                         path="/account/forgot-password"
                         element={<ForgotPassword />}
                     />
-                    // admin route
-                    <Route path="/admin/home" element={<Admin />} />
                 </Routes>
             </div>
         </Router>
