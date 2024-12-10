a6ee0e9b0ac4c84959125dd9ea9ba25d10219f7b
giahao1411 <trangiahao498@gmail.com>
2024-12-10 22:00:26 +0700

    authorization

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes } from "react-router-dom";
 import AccountRoutes from "./routes/AccountRoutes";
 import AdminRoutes from "./routes/AdminRoutes";
 import HomeRoutes from "./routes/HomeRoutes";
+import ErrorRoute from "./routes/ErrorRoute";
 
 const App = () => {
     return (
@@ -12,6 +13,7 @@ const App = () => {
                     {HomeRoutes}
                     {AccountRoutes}
                     {AdminRoutes}
+                    {ErrorRoute}
                 </Routes>
             </div>
         </Router>
